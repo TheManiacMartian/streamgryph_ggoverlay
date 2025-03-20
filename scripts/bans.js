d3c67e7e72037c0388ed4ff615122279b70a8f24
TheManiacMartian <61629906+TheManiacMartian@users.noreply.github.com>
2025-03-20 14:37:35 -0400

    Added bans

---
+++ scripts/bans.js
@@ -0,0 +1,48 @@
+const getJSON = async url => 
+{
+    const response = await fetch(url);
+    if(!response.ok) // check if response worked (no 404 errors etc...)
+      throw new Error(response.statusText);
+  
+    const data = response.json(); // get JSON from the response
+    return data; // returns a promise, which resolves to this data value
+}  
+
+window.onload = init;
+
+function init()
+{
+    let bans = [];
+    
+    async function main()
+    {
+        getJSON("data/match_data.json").then(data => {
+            for(let i = 0; i < data.banPick.length; i++)
+            {
+                document.getElementById(`p${i+1}-char`).src=`images/${data.game}/${data.banPick[i]}.png`;
+                
+                if(bans[i] != data.banPick[i])
+                {
+                    gsap.fromTo(`#p${i+1}-char`,
+                        {
+                            opacity: "0", right:"900px", ease: "power2.out", duration: 4
+                        },
+                        {
+                            opacity: "1", right:"700px", ease: "power2.out", duration: 0.5
+                        }
+                    )
+                }
+
+                
+
+                bans[i] = data.banPick[i];
+            }
+            
+        }).catch(error => {
+            console.error(error);
+        });
+    }
+
+    main();
+    setInterval(() => { main();}, 500); // interval update
+}
